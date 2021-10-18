2f84c596afc87b31abb6d79147d0549ab8ba4f46
mpedraza1 <90863769+mpedraza1@users.noreply.github.com>
2021-10-17 22:03:33 -0300

    archivo profe

---
+++ src/vista/paulo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+/**
+ *esto lo subie el loco del profe
+ * @author User
+ */
+public class paulo {
+    
+}

6fd746587f78fe8f961b40ddc07422c9f266414d
mpedraza1 <90863769+mpedraza1@users.noreply.github.com>
2021-10-19 16:35:20 -0300

    Enlazando vista ModificarBanco

--- src/Controlador/BancoController.java
+++ src/Controlador/BancoController.java
@@ -1,15 +1,10 @@
 package Controlador;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JComboBox;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 import modelo.Banco;
 import modelo.Conexion;
@@ -19,105 +14,78 @@ import vista.ModificarBanco;
 //
 
 public class BancoController {
+     
      static Conexion conn = new Conexion();
-     static Bancos  bc = new Bancos();
+     public static Bancos  bc = new Bancos();
      
 
      public static void mostrar(){
-         bc.setVisible(true);
+        bc.setVisible(true);
      }       
-      
-    public static void btnCrear(String nombre, String status){
-       
+    
+    public static void btnModificar() {
 
-       Conexion conn = new Conexion();
-       
-       
-       boolean guardar = conn.guardarBanco(nombre, status);
+        if (bc.tablaBanco.getSelectedRow() != -1) {
+            
+            String nombre = (String) bc.tablaBanco.getModel().getValueAt(bc.tablaBanco.getSelectedRow(), 0);
+            String id = Banco.ObtenerId(nombre);
+            
+            int estado = (int) bc.tablaBanco.getModel().getValueAt(bc.tablaBanco.getSelectedRow(), 1);
+
+            ModificarBanco modificar = new ModificarBanco(nombre, estado, id);
+            modificar.setVisible(true);
+        }
+      
+      
+        System.out.println(bc.tablaBanco.getModel().getValueAt(bc.tablaBanco.getSelectedRow(), 0));
+        System.out.println(bc.tablaBanco.getModel().getValueAt(bc.tablaBanco.getSelectedRow(), 1));
+    }
+    
+    public static void GuardarModificar(String nombre, int status, String id){
+        Banco.ActualizarDatos(nombre, status, id);
+    
+    
+    }
+    
+    
+        
+     
+    public static void btnCrear(String nombre, int status){
+     
+ 
+       conn.guardarBanco(nombre, status);
        System.out.println(nombre);
-        int estado=0;
-       
-       if(guardar)
-       {
-         estado = 1;
-       }
-          else
-       {
-          estado = 0 ;
-       }
-             System.out.println("este es el estado "+estado);
+      
+             
 
     }
     
-     public void traerBanco(){
-       Conexion conn = new Conexion();
-       Connection con=conn.Conexion();
-       String[] dato = new String[3];
-       String sql = "SELECT * FROM bancos";
-       
-       PreparedStatement pstmt;
-       
-       DefaultTableModel tabla = new DefaultTableModel();
-       JTable tablaBanco = new JTable (tabla);
-       
-       tabla.addColumn("Id");
-       tabla.addColumn("Banco");
-       tabla.addColumn("Estado");
-       
-       tablaBanco.setModel(tabla);
-       
-           
-       try{
-           pstmt = (PreparedStatement) con.createStatement();
-           
-          ResultSet result = pstmt.executeQuery(sql);
-           
-           while (result.next()){
-               dato[0]=result.getString("id_banco");
-               dato[1]=result.getString("nombre_banco");
-               dato[2]=result.getString("estado_banco");
-               
-               tabla.addRow(dato);
-                                             
-           }
-           
-           tablaBanco.setModel(tabla);
-            
-        }catch(SQLException e){
-            
-           
-           
-       }
-    }
-     
+   
     public static ResultSet btnActualizar (String nombre,int estado){
-        if(nombre != "" && estado > 0){
-           DefaultTableModel tblModel = new DefaultTableModel();
+        if(!"".equals(nombre) && estado > 0){
+            DefaultTableModel tblModel = new DefaultTableModel();
            
            Banco banco = new Banco();
            
            ResultSet bncActivos=banco.getBancoActualiza(estado);
-          // ModificarBanco mdfbnc = new ModificarBanco();
-         //  mdfbnc.
+          //ModificarBanco mdfbnc = new ModificarBanco();
+          // mdfbnc.get
            return bncActivos;
         }
          return null;
     }
     
-        
         public static void main(String[] args) {
-             bc.setVisible(true);
+           bc.setVisible(true);
 
         }
         
         
     public static void dataRows(JTable tablaBanco) {
-        DefaultTableModel table = new DefaultTableModel();
+        DefaultTableModel table = (DefaultTableModel) tablaBanco.getModel();
         tablaBanco.setModel(table);
+        table.setRowCount(0);
         
-        
-        table.addColumn("Nombre");
-        table.addColumn("Estado");
         
         
         try{

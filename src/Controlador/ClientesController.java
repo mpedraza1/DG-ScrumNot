7be487b81a349fba4bf6134a86e2145b485e880b
mpedraza1 <90863769+mpedraza1@users.noreply.github.com>
2021-10-21 23:06:45 -0300

    Agregando funcion rellenar tabla

--- src/Controlador/ClientesController.java
+++ src/Controlador/ClientesController.java
@@ -7,6 +7,9 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import modelo.*;
@@ -20,18 +23,24 @@ public class ClientesController {
     public static Cliente cl = new Cliente();
 
     public static void btnAgregar(String nombre, String rut, String celular, String telefono, String correo, String id, Date chosenDate) {
+
         Consultas cs = new Consultas();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String fecha = sdf.format(chosenDate);
-        int celularInt = Integer.parseInt(celular);
-        int telefonoInt = Integer.parseInt(telefono);
-        int idInt = Integer.parseInt(id);
-        cs.doQueryPost("INSERT INTO cliente (rut_cl, nombre_cl, fono_cl, fn_cl, mail_cl, celular_cl, id_red) VALUES ('"+rut+"','"+nombre+"',"+telefonoInt+",'"+fecha+"','"+correo+"',"+celularInt+","+idInt+")");
+        String SQL = "INSERT INTO cliente (rut_cl, nombre_cl, fono_cl, fn_cl, mail_cl, celular_cl,id_red) VALUES('" + rut + "','" + nombre + "'," + telefono + ",'" + fecha + "','" + correo + "'," + celular + ",1)";
         
         
-                
-     //   conn.guardarCliente(nombre, rut, celular, telefono, correo, id);
-          System.out.println("##############################");
+        
+        if (cs.InsertarDatos(SQL)) {
+            JOptionPane.showMessageDialog(null, "Cliente aladido a la base");
+        } else {
+            {
+                JOptionPane.showMessageDialog(null, "Cliente no se pudo añadir a la base");
+            }
+        }
+
+// conn.guardarCliente(nombre, rut, celular, telefono, correo, id);
+        System.out.println("##############################");
 
         System.out.println(fecha);
 
@@ -46,6 +55,41 @@ public class ClientesController {
            return bncActivos;
         }
          return null;*/
+    }
+    
+    
+    public static void Rellenar(){
+    
+        try {
+            
+            Consultas cs = new Consultas();
+            String SQL = "SELECT * FROM CLIENTE";
+            var tabla = (DefaultTableModel) cl.tablaCliente.getModel();
+            tabla.setRowCount(0);
+            String datos[] = new String[7];
+            
+            var rs = cs.PedirDatos(SQL);
+            
+            
+            while (rs.next()) {
+              datos[0] =  rs.getString("nombre_cl");
+              datos[1] =  rs.getString("rut_cl");
+              datos[2] =  rs.getString("fn_cl");
+              datos[3] =  rs.getString("fono_cl");
+              datos[4] =  rs.getString("celular_cl");
+              datos[5] =  rs.getString("mail_cl");
+              datos[6] =  rs.getString("id_red");
+              tabla.addRow(datos);
+            }
+            rs.close();
+            
+            cl.tablaCliente.setModel(tabla);
+            
+            
+        } catch (SQLException ex) {
+            System.out.println("error " + ex);
+        }
+    
     }
 
     public static void mostrar() {
@@ -69,8 +113,6 @@ public class ClientesController {
         System.out.println(cl.tablaCliente.getModel().getValueAt(cl.tablaCliente.getSelectedRow(), 0));
         System.out.println(cl.tablaCliente.getModel().getValueAt(cl.tablaCliente.getSelectedRow(), 1));
     }
-
-   
 
     public static void btnCrear(String nombre, String rut, String celular, String telefono, String correo, String id) {
 
@@ -96,7 +138,7 @@ public class ClientesController {
         cl.setVisible(true);
 
     }
-/*
+    /*
     public static void dataRows(JTable tablaCliente) {
         DefaultTableModel table = (DefaultTableModel) tablaCliente.getModel();
         tablaCliente.setModel(table);
